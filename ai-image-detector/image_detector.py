14233cd362ed53df76e720fa75ffb45b57e494fc
u20040531-dotcom <u20040531@gmail.com>
2025-11-28 23:24:03 +0800

    Update image_detector.py

--- ai-image-detector/image_detector.py
+++ ai-image-detector/image_detector.py
@@ -1,7 +1,7 @@
 import requests
 import sys
 
-HF_TOKEN = "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"
+HF_TOKEN = "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"
 MODEL = "Ateeqq/ai-vs-human-image-detector"
 API_URL = "https://router.huggingface.co/hf-inference/models/" + MODEL
 HEADERS = {"Authorization": f"Bearer {HF_TOKEN}", "Content-Type": "application/octet-stream"}
