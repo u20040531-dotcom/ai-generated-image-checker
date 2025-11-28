899a9d724c1669686ff1c7451ab1f5764b598d31
u20040531-dotcom <u20040531@gmail.com>
2025-11-28 22:59:16 +0800

    Update image_detector.py

--- ai-image-detector/image_detector.py
+++ ai-image-detector/image_detector.py
@@ -9,18 +9,16 @@ HEADERS = {"Authorization": f"Bearer {HF_TOKEN}", "Content-Type": "application/o
 
 
 def predict_image(path):
-with open(path, "rb") as f:
-    img_bytes = f.read()
-    resp = requests.post(API_URL, headers=HEADERS, data=img_bytes, timeout=60)
-return resp.json()
-
-
+    with open(path, "rb") as f:
+        img_bytes = f.read()
+        resp = requests.post(API_URL, headers=HEADERS, data=img_bytes, timeout=60)
+    return resp.json()
+    
 if __name__ == "__main__":
-if len(sys.argv) < 2:
-    print("Usage: python hf_image_detector.py image.jpg")
-    sys.exit(1)
-
-
-result = predict_image(sys.argv[1])
-print("Model Output:")
-print(result)
+    if len(sys.argv) < 2:
+        print("Usage: python hf_image_detector.py image.jpg")
+        sys.exit(1)
+        
+    result = predict_image(sys.argv[1])
+    print("Model Output:")
+    print(result)
