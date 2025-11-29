7d8e55cbb2f2b332da74dc4fa57fd3c16cf89072
u20040531-dotcom <u20040531@gmail.com>
2025-11-29 11:55:12 +0800

    Update image_detector.py

--- ai-image-detector/image_detector.py
+++ ai-image-detector/image_detector.py
@@ -1,16 +1,24 @@
 import requests
 import sys
+import os
 
-HF_TOKEN = "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"
+
+HF_TOKEN = os.getenv("HF_TOKEN")  # 從環境變數讀取
+
+if HF_TOKEN is None:
+    raise ValueError("請先設定環境變數 HF_TOKEN")
+
 MODEL = "Ateeqq/ai-vs-human-image-detector"
 API_URL = "https://router.huggingface.co/hf-inference/models/" + MODEL
 HEADERS = {"Authorization": f"Bearer {HF_TOKEN}", "Content-Type": "application/octet-stream"}
+
 
 def predict_image(path):
     with open(path, "rb") as f:
         img_bytes = f.read()
         resp = requests.post(API_URL, headers=HEADERS, data=img_bytes, timeout=60)
     return resp.json()
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
