bd8e3515c367a762c3f7ccb2788023f2e2426b0d
AI-profile <u20040531@gmail.com>
2025-12-18 12:52:35 +0800

    Update app_streamlit2.py

--- test/app_streamlit2.py
+++ test/app_streamlit2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 from image_detector import ImageDetector
-
+import sys
 st.title("AI vs Human 圖片識別系統")
 
 detector = ImageDetector("ai_image_detector.pth")
@@ -12,12 +12,12 @@ uploaded = st.file_uploader(
 )
 
 if uploaded:
-    img = Image.open(uploaded).convert("RGB")
+    img = Image.open(sys.argv[1]).convert("RGB")
     st.image(img, use_column_width=True)
 
     if st.button("跑動測試"):
         prob = detector.predict(img)
-
+        print(prob)
         labels = ["AI Generated", "Real Image"]
         result = labels[prob.argmax()]
 
